e1a1c35e63464502165f6ac18072ad367a0fce26
ilkay-bicici <ilkaybicici01@gmail.com>
2024-06-01 23:08:36 +0300

    danisman ogrencilerini goruntuleme ozelligi eklendi

--- FinalTestProject/Data/Ogrenci.cs
+++ FinalTestProject/Data/Ogrenci.cs
@@ -1,11 +1,15 @@
 ﻿using FinalTestProject.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinalTestProject.Data
 {
     public class Ogrenci : Hesap
     {
         public List<string>? SecilmisDersler { get; set; }
-        public string? OgrenciDanismani { get; set; }
+
+        [ForeignKey("Danisman")]
+        public long? OgrenciDanismani { get; set; }
+        public Danisman OgrDanisman { get; set; }
         public int? Sinif { get; set; }
         public int? Donem { get; set; }
 
---
+++ FinalTestProject/Migrations/20240601194932_AddForeignKeyToOgrenci.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalTestProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToOgrenci : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "OgrenciDanismani",
+                table: "Ogrenci",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ogrenci_OgrenciDanismani",
+                table: "Ogrenci",
+                column: "OgrenciDanismani");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ogrenci_Danisman_OgrenciDanismani",
+                table: "Ogrenci",
+                column: "OgrenciDanismani",
+                principalTable: "Danisman",
+                principalColumn: "TCKimlikNo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ogrenci_Danisman_OgrenciDanismani",
+                table: "Ogrenci");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ogrenci_OgrenciDanismani",
+                table: "Ogrenci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OgrenciDanismani",
+                table: "Ogrenci",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
